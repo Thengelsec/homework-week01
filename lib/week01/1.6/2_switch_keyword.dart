66ffd1ac5c8e77190d1b3dc4b59d3c8856e2d898
thengelsec <ssuk83158315@gmail.com>
2025-09-05 23:24:44 +0900

    Fix: Add missing break statement in switch case

--- lib/week01/1.6/2_switch_keyword.dart
+++ lib/week01/1.6/2_switch_keyword.dart
@@ -9,7 +9,9 @@ void main() {
 
   switch (status) {
     case Status.approved:
-    // approved값이기 때문에 다음 코드가 실행됩니다.
-    print('승인 상태입니다.');
+      // approved값이기 때문에 다음 코드가 실행됩니다.
+      print('승인 상태입니다.');
+      break;
+    
   }
 }
