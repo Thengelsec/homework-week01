cae7e3ce03eb8a83bea18c8b84ecb63475f52469
thengelsec <ssuk83158315@gmail.com>
2025-09-14 15:34:12 +0900

    feat(week02): Add generic cache example with List<int>

--- lib/week02/2.8/1_generic.dart
+++ lib/week02/2.8/1_generic.dart
@@ -11,4 +11,13 @@ class Cache<T> {
   // reduce() 함수가 기억나지 않는다면 1.4.1절 'List 타입'을 복습하세요.
   print(cache.data.reduce((value, element) => value + element));
 }
+void main() {
+  // T의 타입을 List<int>로 입력합니다.
+  final cache = Cache<List<int>>(
+    data: [1,2,3],
+  );
 
+  // 제네릭에 입력된 값을통해 data 변수의 타입이 자동으로 유추됩니다.
+  // 1.3.1절 List타입의 reduce() 함수 참고
+  print(cache.data.reduce((value, element) => value + element));
+}
