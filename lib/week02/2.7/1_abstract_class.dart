609880bc7b4d4769d4db3c9391226b9d3cf5023c
thengelsec <ssuk83158315@gmail.com>
2025-09-14 15:26:42 +0900

    feat: Define abstract methods sayName and sayMembersCount in Idol class.

--- lib/week02/2.7/1_abstract_class.dart
+++ lib/week02/2.7/1_abstract_class.dart
@@ -17,5 +17,8 @@ abstract class Idol {
   final String name;
   final int membersCount;
 
-  Idol(this.name, this.membersCount); // 생성자 선
+  Idol(this.name, this.membersCount); // 생성자 선언
+
+  void sayName();           // 추상 메서드 선언
+  void sayMembersCount();   // 추상 메서드 선언
 }
