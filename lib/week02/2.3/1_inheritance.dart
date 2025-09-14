f3c8fb5734ea525963b2daddc8169dc1e58cea22
thengelsec <ssuk83158315@gmail.com>
2025-09-14 15:05:42 +0900

    feat: Add inheritance example with BoyGroup extending Idol

--- lib/week02/2.3/1_inheritance.dart
+++ lib/week02/2.3/1_inheritance.dart
@@ -15,6 +15,8 @@ class Idol {
 // extends 키워드를 사용해서 상속받습니다.
 // class 자식 클래스 extends 부모 클래스 순서입니다.
 class BoyGroup extends Idol {
+
+  // 상속받은 생성자
   BoyGroup(
     String name,
     int membersCount,
@@ -23,7 +25,16 @@ class BoyGroup extends Idol {
     membersCount,
   );
 
+  // 상속받지 않은 기능
   void sayMale() {
     print('저는 남자 아이돌입니다.');
   }
+}
+void main() {
+
+  BoyGroup bts = BoyGroup('BTS', 7);  // 생성자로 객체 생성
+
+  bts.sayName();          // 부모한테 물려받은 메서드
+  bts.sayMembersCount();  // 부모한테 물려받은 메서드
+  bts.sayMale();          // 자식이 새로 추가한 메서드
 }
