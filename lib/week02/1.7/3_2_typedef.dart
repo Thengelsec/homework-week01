6e9a98fac19c0f45bb2525e9233157c94f3a0161
thengelsec <ssuk83158315@gmail.com>
2025-09-14 14:52:41 +0900

    feat: Implement typedef example with add and subtract functions

---
+++ lib/week02/1.7/3_2_typedef.dart
@@ -0,0 +1,13 @@
+typedef Operation = void Function(int x, int y);
+
+void add(int x, int y) {
+  print('결과값 : ${x + y}');
+}
+
+void subtract(int x, int y) {
+  print('결과값 : ${x - y}');
+}
+
+void main() {
+  
+}
