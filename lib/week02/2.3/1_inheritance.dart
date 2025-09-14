fa1e25ba495d05326f89999e618c6d7975555ef2
thengelsec <ssuk83158315@gmail.com>
2025-09-14 15:04:12 +0900

    feat(inheritance): Implement BoyGroup class inheriting from Idol

--- lib/week02/2.3/1_inheritance.dart
+++ lib/week02/2.3/1_inheritance.dart
@@ -12,3 +12,18 @@ class Idol {
     print('${this.name} 멤버는 ${this.membersCount}명입니다.');
   }
 }
+// extends 키워드를 사용해서 상속받습니다.
+// class 자식 클래스 extends 부모 클래스 순서입니다.
+class BoyGroup extends Idol {
+  BoyGroup(
+    String name,
+    int membersCount,
+  ) : super(    // super는 무보 클래스를 지칭합니다.
+    name,
+    membersCount,
+  );
+
+  void sayMale() {
+    print('저는 남자 아이돌입니다.');
+  }
+}
