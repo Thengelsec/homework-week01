e288f32a6746e95d452dd0199d9114be80b71841
thengelsec <ssuk83158315@gmail.com>
2025-09-14 15:12:12 +0900

    feat: Implement GirlGroup class with override

--- lib/week02/2.4/1_override.dart
+++ lib/week02/2.4/1_override.dart
@@ -12,3 +12,17 @@ class Idol {
     print('${this.name} 멤버는 ${this.membersCount}명입니다.');
   }
 }
+class GirlGroup extends Idol {
+  // 2.3 상속에서처럼 super 키워드를 사용해도 되고 다음처럼 생성자의 매개변수로
+  // 직접 super 키워드를 사용해도 됩니다.
+  GirlGroup(
+    super.name,
+    super.membersCount,
+  );
+
+  // override 키워드를 사용해 오버라이드합니다.
+  @override
+  void sayName() {
+    print('저는 여자 아이돌 ${this.name}입니다.');
+  }
+}
