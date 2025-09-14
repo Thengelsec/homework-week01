5ecce88b89c3957743d150abda342ed56949bde0
thengelsec <ssuk83158315@gmail.com>
2025-09-14 15:25:42 +0900

    Refactor: Convert Idol class to abstract class

---
+++ lib/week02/2.7/1_abstract_class.dart
@@ -0,0 +1,21 @@
+class Idol {
+  final String name;
+  final int membersCount;
+
+  Idol(this.name, this.membersCount);
+
+  void sayName() {
+    print('저는 ${this.name}입니다.');
+  }
+
+  void sayMembersCount() {
+    print('${this.name} 멤버는 ${this.membersCount}명입니다.');
+  }
+}
+// abstract 키워드를 사용해 추상 클래스 지정
+abstract class Idol {
+  final String name;
+  final int membersCount;
+
+  Idol(this.name, this.membersCount); // 생성자 선언
+}
