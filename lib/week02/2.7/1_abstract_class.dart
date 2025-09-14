80fcf9e05f01ddcff498ce8bc4bcba1a76309506
thengelsec <ssuk83158315@gmail.com>
2025-09-14 15:28:42 +0900

    feat(abstract_class): Implement GirlGroup class based on Idol abstract class

--- lib/week02/2.7/1_abstract_class.dart
+++ lib/week02/2.7/1_abstract_class.dart
@@ -25,5 +25,18 @@ abstract class Idol {
 // implements 키워드를 사용해 추상 클래스를 구현하는 클래스
 class GirlGroup implements Idol {
   final String name;
-  
+  final int membersCount;
+
+  GirlGroup(
+    this.name,
+    this.membersCount,
+  );
+
+  void sayName() {
+    print('저는 여자 아이돌 ${this.name}입니다.');
+  }
+
+  void sayMembersCount() {
+    print('${this.name} 멤버는 ${this.membersCount}명입니다.');
+  }
 }
