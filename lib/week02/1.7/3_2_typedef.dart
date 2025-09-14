4d36d8d803ab104395da02249e55720fe472de25
thengelsec <ssuk83158315@gmail.com>
2025-09-14 14:54:42 +0900

    feat(typedef): Demonstrate typedef function reassignment

--- lib/week02/1.7/3_2_typedef.dart
+++ lib/week02/1.7/3_2_typedef.dart
@@ -11,5 +11,10 @@ void subtract(int x, int y) {
 void main() {
   // typedef는 일반적인 변수의 type처럼 사용 가능
   Operation oper = add;
+  oper(1, 2);
 
+  // subtract() 함수도 Operation에 해당되는
+  // 시그니처이므로 oper 변수에 저장 가능
+  oper = subtract;
+  oper(1, 2);
 }
